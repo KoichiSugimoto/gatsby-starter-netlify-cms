2be5abb41e472a3c76be9720563373646441f4c0
sugimoto <info@koichi.su>
2019-02-23 01:39:15 +0900

    styles static pages

---
+++ html/stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-primer",
+    "stylelint-config-rational-order",
+    "./node_modules/prettier-stylelint/config.js"
+  ],
+  rules: {
+    indentation: 2
+  }
+};
